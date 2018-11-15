1c2508941d1494df9af86560f9cffd819bd8e2f2
SaravananAyyanar <44993310+SaravananAyyanar@users.noreply.github.com>
2018-11-15 09:49:29 +0530

    how-to-select-a-column-in-winforms-datagrid

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.WinForms.Controls;
+using Syncfusion.WinForms.DataGrid.Styles;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGrid.Enums;
+
+namespace GettingStarted
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var data = new OrderInfoCollection();
+            sfDataGrid.DataSource = data.OrdersListDetails;
+
+            this.sfDataGrid.AllowSorting = false;
+            this.sfDataGrid.SelectionUnit = SelectionUnit.Cell;
+            this.sfDataGrid.SelectionMode = GridSelectionMode.Extended;
+            this.sfDataGrid.CellClick += sfDataGrid_CellClick;
+        }
+
+        void sfDataGrid_CellClick(object sender, Syncfusion.WinForms.DataGrid.Events.CellClickEventArgs e)
+        {
+            if (e.DataRow.RowType == RowType.HeaderRow && this.sfDataGrid.View.TopLevelGroup == null)
+            {
+                var firstRowDate = this.sfDataGrid.View.Records[0];
+                var lastRowData = this.sfDataGrid.View.Records[this.sfDataGrid.View.Records.Count - 1];
+                var column = e.DataColumn.GridColumn;
+
+                if (firstRowDate != null && lastRowData != null)
+                {
+                    this.sfDataGrid.ClearSelection();
+                    this.sfDataGrid.SelectCells(firstRowDate, column, lastRowData, column);
+                }
+            }
+        }
+
+    }
+}
